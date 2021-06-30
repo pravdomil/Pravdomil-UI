5af88663d381502657b07b61d420c8026a0bc7f4
pravdomil <pravdomil.toman@gmail.com>
2021-06-30 16:52:53 +0200

    add inputColor

--- src/Main.elm
+++ src/Main.elm
@@ -71,7 +71,7 @@ baseFunctions =
 
         inputAttr : String
         inputAttr =
-            "(spacing A.inputSpacing :: padding A.inputPadding :: bgColor A.inputBgColor :: borderColor A.inputBorderColor :: borderWidth A.inputBorderWidth :: borderRounded A.inputBorderRounded :: a)"
+            "(spacing A.inputSpacing :: padding A.inputPadding :: bgColor A.inputBgColor :: fontColor A.inputColor :: borderColor A.inputBorderColor :: borderWidth A.inputBorderWidth :: borderRounded A.inputBorderRounded :: a)"
     in
     [ "rem a = round (A.rootEm * a)"
     , "rem_ a = A.rootEm * a"
