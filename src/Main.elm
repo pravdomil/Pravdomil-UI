8e7b05642b45ec711f6264b3d87f90704fe2995a
pravdomil <pravdomil.toman@gmail.com>
2021-06-30 13:20:33 +0200

    shadow123

--- src/Main.elm
+++ src/Main.elm
@@ -85,6 +85,10 @@ baseFunctions =
     , "h5 a = p (Region.heading 5 :: fontSize A.h5FontSize :: a)"
     , "h6 a = p (Region.heading 6 :: fontSize A.h6FontSize :: a)"
     , ""
+    , "shadow1 = A.shadow1"
+    , "shadow2 = A.shadow2"
+    , "shadow3 = A.shadow3"
+    , ""
     , "br = html (Html.br [] [])" -- https://github.com/mdgriffith/elm-ui/issues/276
     , "hr = el [ width fill, paddingXY 0 (rem 1) ] (el [ width fill, borderWidthEach 0 0 0 1, borderColor A.hrBorderColor ] none)"
     , ""
