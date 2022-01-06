9611ab48c22a2ef575e2d848a705f7bf4786cf5c
pravdomil <pravdomil.toman@gmail.com>
2022-01-06 15:45:16 +0100

    remove adaptiveScale

--- src/Main.elm
+++ src/Main.elm
@@ -114,8 +114,6 @@ baseFunctions =
     , ""
     , "inputPlaceholder a = Element.Input.placeholder (fontColor A.placeholderColor :: fontSize A.placeholderFontSize :: a)"
     , ""
-    , "adaptiveScale = Html.node \"style\" [] [ Html.text \"@media screen and (pointer: fine) { body { zoom: 0.875 } }\" ]"
-    , ""
     , "--"
     , ""
     ]
